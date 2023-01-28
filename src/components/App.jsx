64184fe4db1184b12dc1f3676d5340003b60c275
Lina Budkova <linabudkova35@gmail.com>
2023-01-28 13:55:44 +0200

    wqeqwe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Profile } from "./Profile/Profile";
+import user from './json/user'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile key={ user.username} username={user.username} tag={user.tag} location={user.location} avatar = {user.avatar} followers={user.stats.followers} views = {user.stats.views} likes = {user.stats.likes} />
     </div>
   );
 };
